8224331143864456a89af4d80e3c2976905c63b4
George <std159243@ac.eap.gr>
2023-03-06 23:38:52 +0200

    Images Update/ Credits button functionality update

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,26 +1,38 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 # Για εγκατάσταση του PIL τρέχουμε  pip install Pillow στο terminal
 from PIL import Image, ImageTk
 
 
-
-
-
-
+# Συνάρτηση που καλείται μόλις πατηθεί το κουμπί "Credits"
 def open_credits_window():
     # Δημιουργία popup παράθυρου credits
     credits_window = tk.Toplevel()
     credits_window.title("Credits")
+    credits_window.geometry("700x420+100+100")  # διαστάσεις και θέση παράθυρου 'credits_window' στην οθόνη
 
-    credits_window.config(width=700, height=400)
+    # Φόρτωση της εικόνας φόντου
+    bg = PhotoImage(file=r"./images/python.png")
+    # δημιουργία αντικείμενου 'canvas' στο 'credits_window' με συγκεκριμένες διαστάσεις
+    my_canvas = Canvas(credits_window, width=500, height=420)
+    # τοποθέτηση του widget 'my_canvas' σε όλο το παράθυρο
+    my_canvas.pack(fill="both", expand=True)
+    # τοποθέτηση εικόνας bg στο παράθυρο με αγκίστρωση πάνω αριστερά
+    my_canvas.create_image(0, 0, image=bg, anchor="nw")
 
-    #Δημιουργία button για κλείσιμο παραθύρου credits.
-    button_back = tk.Button(credits_window, image=Back_photo, command=credits_window.destroy)
-    button_back.image=Back_photo
-    button_back.place(x=500, y=240)
+    # Δημιουργία κειμένου στο my_canvas
+    my_canvas.create_text(290, 10, text="TicTacToe Project\n\nThis project was created as part\nof the PLIPRO "
+                                        "thematic unit\nof the Hellenic Open University.\n\nCreators "
+                                        "in alphabetical order are:\nMallis Georgios\nPeppas Georgios\n"
+                                        "Pierrakou Konstantina\nPrimpas Efstratios\nStilianou Stelios", anchor=NW,
+                          font=("Helvetica", 14, 'bold'))
 
+    # δημιουργία κουμπιού 'button_back' που κλείνει το παράθυρο credits_window
+    button_back = Button(credits_window, image=Back_photo, command=credits_window.destroy)
+    my_canvas.create_window(0, 420, anchor="sw", window=button_back)
 
+    # εκτέλεση βρόγχου
+    credits_window.mainloop()
 
 
 # Δημιουργία του αρχικού παραθύρου
@@ -49,7 +61,7 @@ HallOfFame_photo = ImageTk.PhotoImage(HallOfFame_image)
 Exit_image = Image.open(r"./images/Exit.png")
 Exit_photo = ImageTk.PhotoImage(Exit_image)
 Back_image = Image.open(r"./images/back.png")
-Back_photo=ImageTk.PhotoImage(Back_image)
+Back_photo = ImageTk.PhotoImage(Back_image)
 
 # Δημιουργία των 3 διαφορετικών κουμπιών με τύπο αντιπάλων / οριζόντια τοποθέτηση
 human_vs_human_button = tk.Button(button_frame, image=human_vs_human_photo)
@@ -74,7 +86,7 @@ pc_vs_pc_label = tk.Label(button_frame, text="PC Vs PC")
 pc_vs_pc_label.grid(row=1, column=2)
 
 # Δημιουργία των κουμπιών Credits, Hall of Fame, Exit / οριζόντια τοποθέτηση
-credits_button = tk.Button(button_frame, image=credits_photo,command=open_credits_window)
+credits_button = tk.Button(button_frame, image=credits_photo, command=open_credits_window)
 credits_button.image = credits_photo
 credits_button.grid(row=2, column=0, padx=5, pady=20)
 
